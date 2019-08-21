e333eed2d3186294248a58c8c5334e321368e0eb
Will0376 <lbvrf090@gmail.com>
2019-08-21 17:39:17 +0300

    Начало с нуля. подготовка

--- src/main/java/ru/will0376/xBlocker/Main.java
+++ src/main/java/ru/will0376/xBlocker/Main.java
@@ -48,7 +48,7 @@ public class Main {
 	public static final String MODID = "xblocker";
 	public static final String NAME = "xBlocker";
 	public static final String VERSION = "1.0.7";
-	public static final boolean FOR_SERVER = false;
+	public static final boolean FOR_SERVER = true;
 	public ArrayList<String> listItemsClient = Lists.newArrayList();
 	public ArrayList<String> listTempClient = Lists.newArrayList();
 	public ArrayList<String> listEnchantClient = Lists.newArrayList();
